a5ff54598528a7b830d9c409195af49f524c1514
Sereja <Ignatushinsa@yandex.ru>
2026-07-14 14:23:36 +0300

    string parameters fix #67
    Для сокращения кода использовал конструкцию (* use_dsp = USE_DSP_VALUE ? "yes" : "no" *), как я понял должно работать, т.к. уже находится в блоке generate. Синтез и имплементация проходят.

--- lib/mult/src/mult.sv
+++ lib/mult/src/mult.sv
@@ -4,51 +4,30 @@
 module mult #(
     parameter A_WIDTH = 8,
     parameter B_WIDTH = 8,
-    parameter USE_DSP_VALUE = 1'b1,
-    parameter SIGNED_OPERANDS = 1'b1
+    parameter int USE_DSP_VALUE = 1,
+    parameter int SIGNED_OPERANDS = 1
 )(
     input  logic clk,
     input  logic [A_WIDTH-1:0] a,
     input  logic [B_WIDTH-1:0] b,
     output logic [A_WIDTH+B_WIDTH-1:0] result
 );
-    
+
     generate
         if (SIGNED_OPERANDS) begin : gen_signed
-            if (USE_DSP_VALUE) begin : gen_use_dsp_yes
-                (* use_dsp = "yes" *)
-                logic signed [A_WIDTH+B_WIDTH-1:0] result_next;
-                assign result_next = $signed(a) * $signed(b);
-                always_ff @(posedge clk) begin
-                    result <= result_next;
-                end
-            end else begin : gen_use_dsp_no
-                (* use_dsp = "no" *)
-                logic signed [A_WIDTH+B_WIDTH-1:0] result_next;
-                assign result_next = $signed(a) * $signed(b);
-                always_ff @(posedge clk) begin
-                    result <= result_next;
-                end
+            (* use_dsp = USE_DSP_VALUE ? "yes" : "no" *)
+            logic signed [A_WIDTH+B_WIDTH-1:0] result_next;
+            assign result_next = $signed(a) * $signed(b);
+            always_ff @(posedge clk) begin
+                result <= result_next;
             end
-            
-        end
-        else begin : gen_unsigned
-            if (USE_DSP_VALUE) begin : gen_use_dsp_yes
-                (* use_dsp = "yes" *)
-                logic [A_WIDTH+B_WIDTH-1:0] result_next;
-                assign result_next = a * b;
-                always_ff @(posedge clk) begin
-                    result <= result_next;
-                end
-            end else begin : gen_use_dsp_no
-                (* use_dsp = "no" *)
-                logic [A_WIDTH+B_WIDTH-1:0] result_next;
-                assign result_next = a * b;
-                always_ff @(posedge clk) begin
-                    result <= result_next;
-                end
+        end else begin : gen_unsigned
+            (* use_dsp = USE_DSP_VALUE ? "yes" : "no" *)
+            logic [A_WIDTH+B_WIDTH-1:0] result_next;
+            assign result_next = a * b;
+            always_ff @(posedge clk) begin
+                result <= result_next;
             end
         end
     endgenerate
 endmodule
-
